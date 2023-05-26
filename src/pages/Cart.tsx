1526c8851ad2b600911f9a7c957c9d1676da3f88
damiansarnecki <xdamiansarnecki@gmail.com>
2023-05-26 20:01:28 +0200

    added empty cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,8 +1,17 @@
-import React from "react";
-import { PageContainer } from "../components/shared/containers/PageContainer";
-import { useUserContext } from "../provider/user/UserContext";
+import React from 'react';
+import { EmptyCart } from '../components/pages/cart/EmptyCart/emptyCart';
+import { PageContainer } from '../components/shared/containers/PageContainer';
+import { Heading } from '../components/shared/Heading';
+import { useCartContext } from '../provider/cart/CartContext';
+import { useUserContext } from '../provider/user/UserContext';
 
 export const Cart = () => {
+  const { cartData } = useCartContext();
 
-  return <PageContainer>Cart</PageContainer>;
+  return (
+    <PageContainer>
+      <Heading text={"Your shopping cart"}/>
+      <>{cartData.length > 0 ? <></> : <EmptyCart />}</>
+    </PageContainer>
+  );
 };
