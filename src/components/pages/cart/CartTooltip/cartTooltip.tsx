2519d229d973494fa4ca1e3fbc51a5ccf31b7eb2
Patryk Rossa <patryk.rossaa@gmail.com>
2023-05-27 08:45:16 +0200

    fix: cart tooltip

--- src/components/pages/cart/CartTooltip/cartTooltip.tsx
+++ src/components/pages/cart/CartTooltip/cartTooltip.tsx
@@ -1,13 +1,34 @@
-import { Box, Button, Flex } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
-import { HeadingSmall } from '../../../shared/HeadingSmall';
+import { Box, Button, Flex } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { HeadingSmall } from "../../../shared/HeadingSmall";
 
 export const CartTooltip = () => {
   return (
-    <Flex display="none"  w="auto" paddingY="50px" right='0' top="0px" _groupHover={{ display: 'block' }} pos="absolute" zIndex="1000">
-      <Flex align="center" borderRadius='8px' padding="29px 33px" border="1px solid" borderColor='rgba(255, 255, 255, 0.17)' right='0' flexDir="column" bg="rgba(0, 0, 0, 0.3)">
+    <Flex
+      display="none"
+      w="auto"
+      paddingY="50px"
+      right="0"
+      top="0px"
+      _groupHover={{ display: "block" }}
+      pos="absolute"
+      zIndex="1000"
+      opacity="1"
+    >
+      <Flex
+        align="center"
+        borderRadius="8px"
+        padding="29px 33px"
+        border="1px solid"
+        borderColor="rgba(255, 255, 255, 0.17)"
+        right="0"
+        flexDir="column"
+        bg="backgroundMain"
+      >
         <HeadingSmall text="Your  cart is empty" />
-        <Box fontSize="16px" color="white">Looking for inspiration?</Box>
+        <Box fontSize="16px" color="white">
+          Looking for inspiration?
+        </Box>
         <Link to="/marketplace">
           <Button px="70px" mt="30px">
             Go to marketplace
