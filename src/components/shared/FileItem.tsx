e48b2ff10a48f1197903deae7d8ead44a358ea52
Patryk Rossa <patryk.rossaa@gmail.com>
2023-05-27 10:02:49 +0200

    fix: listings skeleton

--- src/components/shared/FileItem.tsx
+++ src/components/shared/FileItem.tsx
@@ -1,7 +1,8 @@
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Skeleton, Text } from "@chakra-ui/react";
 import React, { FC } from "react";
 import { BookmarkIcon } from "../../icons/BookmarkIcon";
 import { EyeIcon } from "../../icons/EyeIcon";
+import { useListingContext } from "../../provider/listings/ListingsContext";
 import { currencyFormatter } from "../../utils/currencyFormatter";
 
 export interface IFileItem {
@@ -23,46 +24,62 @@ export const FileItem: FC<IFileItem> = ({
   peers,
   isSmall
 }) => {
+  const { loading, listings } = useListingContext();
   return (
-    <Flex
-      bgGradient="linear(180deg, #282939 0%, rgba(40, 41, 57, 0.51) 100%)"
-      p={isSmall ? "13px 11px" : "17px 15px"}
-      borderRadius="17px"
-      fontFamily="Inter"
-      flexDir="column"
-      gap="12px"
-    >
-      <Flex gap="7px" align="center">
-        <BookmarkIcon />
-        <Text fontSize="10px" fontWeight="500" color="textSecondary">
-          {category}
-        </Text>
-      </Flex>
-      <Image
-        src={imageUrl}
-        minW={isSmall ? "200px" : "268px"}
-        minH={isSmall ? "150px" : "200px"}
-        maxW={isSmall ? "200px" : "268px"}
-        maxH={isSmall ? "150px" : "200px"}
-        borderRadius="8px"
-      />
-      <Text fontSize={isSmall ? "14px" : "16px"} fontWeight="700">
-        {name.length > 27 ? name.slice(0, 27) + "..." : name}
-      </Text>
-      <Text mt="5px" fontSize="14px" fontWeight="600" color="brandPrimary">
-        {currencyFormatter(price)}
-      </Text>
-      <Box my="5px" h="1px" w="100%" bgColor="#383A56" />
-      <Flex justify="space-between" align="center">
-        <EyeIcon />
-        <Flex gap="19px" align="center" fontSize="12px" color="textSecondary">
-          <Text fontWeight="600">{size} GB</Text>
-          <Flex fontWeight="600">
-            <Text fontWeight="300">Peers:&nbsp;</Text>
-            <Text>{peers}</Text>
+    <>
+      {loading ? (
+        <Skeleton
+          h={isSmall ? "270px" : "360px"}
+          minW={isSmall ? "220px" : "300px"}
+          borderRadius="17px"
+        />
+      ) : (
+        <Flex
+          bgGradient="linear(180deg, #282939 0%, rgba(40, 41, 57, 0.51) 100%)"
+          p={isSmall ? "13px 11px" : "17px 15px"}
+          borderRadius="17px"
+          fontFamily="Inter"
+          flexDir="column"
+          gap="12px"
+        >
+          <Flex gap="7px" align="center">
+            <BookmarkIcon />
+            <Text fontSize="10px" fontWeight="500" color="textSecondary">
+              {category}
+            </Text>
+          </Flex>
+          <Image
+            src={imageUrl}
+            minW={isSmall ? "200px" : "268px"}
+            minH={isSmall ? "150px" : "200px"}
+            maxW={isSmall ? "200px" : "268px"}
+            maxH={isSmall ? "150px" : "200px"}
+            borderRadius="8px"
+          />
+          <Text fontSize={isSmall ? "14px" : "16px"} fontWeight="700">
+            {name.length > 27 ? name.slice(0, 27) + "..." : name}
+          </Text>
+          <Text mt="5px" fontSize="14px" fontWeight="600" color="brandPrimary">
+            {currencyFormatter(price)}
+          </Text>
+          <Box my="5px" h="1px" w="100%" bgColor="#383A56" />
+          <Flex justify="space-between" align="center">
+            <EyeIcon />
+            <Flex
+              gap="19px"
+              align="center"
+              fontSize="12px"
+              color="textSecondary"
+            >
+              <Text fontWeight="600">{size} GB</Text>
+              <Flex fontWeight="600">
+                <Text fontWeight="300">Peers:&nbsp;</Text>
+                <Text>{peers}</Text>
+              </Flex>
+            </Flex>
           </Flex>
         </Flex>
-      </Flex>
-    </Flex>
+      )}
+    </>
   );
 };
