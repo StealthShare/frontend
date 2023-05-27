7bb1f6f4cc2028b2f51a33c5f3bb4528846750bf
Patryk Rossa <patryk.rossaa@gmail.com>
2023-05-27 14:26:31 +0200

    inventory view

--- src/provider/cart/CartContext.tsx
+++ src/provider/cart/CartContext.tsx
@@ -1,20 +1,22 @@
-import axios from 'axios';
-import React, { createContext, useContext, useReducer } from 'react';
-import useLocalStorage from 'use-local-storage';
-import { API_URL } from '../../constants';
-import { cartReducer } from './cartReducer';
-import { ethers } from 'ethers';
+import axios from "axios";
+import React, { createContext, useContext, useReducer } from "react";
+import useLocalStorage from "use-local-storage";
+import { API_URL } from "../../constants";
+import { cartReducer } from "./cartReducer";
+import { ethers } from "ethers";
 
 interface IUserContext {
   cartData: any[] | null;
   deleteItemByAddress: (address: string) => void;
-  clearCart: ()=> void;
+  clearCart: () => void;
+  addItemToCart: (data: any) => void;
 }
 
 const initValue: IUserContext = {
   cartData: [],
   deleteItemByAddress: (address: string) => {},
-  clearCart: () => {}
+  clearCart: () => {},
+  addItemToCart: () => {}
 };
 
 export interface CartItem {
@@ -29,51 +31,65 @@ export interface CartItem {
 
 const CartContext = createContext(initValue);
 
-export const CartContextProvider = ({ children }: { children: React.ReactNode }) => {
+export const CartContextProvider = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
   const [{}, dispatchUser] = useReducer(cartReducer, initValue);
-  const [cartData, setCartData] = useLocalStorage<any[] | null>('CART', [
+  const [cartData, setCartData] = useLocalStorage<any[] | null>("CART", [
     {
       amount: 1,
-      address: '12',
-      imageUrl: '/assets/samples/1.png',
-      category: 'Gaming',
-      name: 'Some shit',
+      address: "12",
+      imageUrl: "/assets/samples/1.png",
+      category: "Gaming",
+      name: "Some shit",
       price: 22,
-      size: 1.2,
+      size: 1.2
     },
     {
       amount: 1,
-      imageUrl: '/assets/samples/1.png',
-      address: '34',
-      category: 'Something',
-      name: 'Elo elo 320',
+      imageUrl: "/assets/samples/1.png",
+      address: "34",
+      category: "Something",
+      name: "Elo elo 320",
       price: 40,
-      size: 0.1,
-    },
+      size: 0.1
+    }
   ]);
 
   const clearCart = () => {
-    setCartData(null)
-  }
+    setCartData(null);
+  };
+
+  const addItemToCart = (data: any) => {
+    setCartData((prevState: any) => [...prevState, data]);
+  };
 
   const deleteItemByAddress = (address: string) => {
-    if(cartData?.length == 1) {
-      setCartData(null)
+    if (cartData?.length == 1) {
+      setCartData(null);
     } else {
-      setCartData(cartData?.filter((item) => item.address != address))
+      setCartData(cartData?.filter((item) => item.address != address));
     }
-  
-    
-  }
+  };
 
-  return <CartContext.Provider value={{ cartData, clearCart, deleteItemByAddress }}>{children}</CartContext.Provider>;
+  return (
+    <CartContext.Provider
+      value={{ cartData, clearCart, deleteItemByAddress, addItemToCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 };
 
 export const useCartContext = () => {
   const context = useContext(CartContext);
 
   if (!context) {
-    throw new Error('`useCartProvider` hook cannot be used outside of a `CartProvider`!');
+    throw new Error(
+      "`useCartProvider` hook cannot be used outside of a `CartProvider`!"
+    );
   }
   return context;
 };
