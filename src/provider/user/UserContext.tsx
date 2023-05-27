d569d1cc848a8d8c215d246d38910a23b2bbbe01
Patryk Rossa <patryk.rossaa@gmail.com>
2023-05-27 03:16:13 +0200

    upload

--- src/provider/user/UserContext.tsx
+++ src/provider/user/UserContext.tsx
@@ -1,9 +1,9 @@
-import axios from 'axios';
-import React, { createContext, useContext, useReducer } from 'react';
-import useLocalStorage from 'use-local-storage';
-import { API_URL } from '../../constants';
-import { userReducer } from './userReducer';
-import { ethers } from 'ethers';
+import axios from "axios";
+import React, { createContext, useContext, useReducer } from "react";
+import useLocalStorage from "use-local-storage";
+import { API_URL } from "../../constants";
+import { userReducer } from "./userReducer";
+import { ethers } from "ethers";
 
 interface IUserContext {
   isLoggedIn: boolean;
@@ -15,55 +15,67 @@ interface IUserContext {
 
 const initValue: IUserContext = {
   isLoggedIn: false,
-  address: '',
+  address: "",
   login: () => {},
   logout: () => {},
-  jwt: null,
+  jwt: null
 };
 
 const UserContext = createContext(initValue);
 
-export const UserContextProvider = ({ children }: { children: React.ReactNode }) => {
-  const [{ isLoggedIn, address }, dispatchUser] = useReducer(userReducer, initValue);
-  const [userAddress, setUserAddress] = useLocalStorage<string | null>('address', null);
-  const [jwt, setJWT] = useLocalStorage<string | null>('JWT', null);
+export const UserContextProvider = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  const [{ isLoggedIn, address }, dispatchUser] = useReducer(
+    userReducer,
+    initValue
+  );
+  const [userAddress, setUserAddress] = useLocalStorage<string | null>(
+    "address",
+    null
+  );
+  const [jwt, setJWT] = useLocalStorage<string | null>("JWT", null);
 
   const login = async () => {
     try {
       await (window as any).ethereum.enable();
       const provider = new ethers.BrowserProvider((window as any).ethereum);
 
-      const accounts = await (window as any).ethereum.request({ method: 'eth_requestAccounts' });
+      const accounts = await (window as any).ethereum.request({
+        method: "eth_requestAccounts"
+      });
       const account = accounts[0];
 
       const seedResponse = await axios.get(`${API_URL}/${account}/getNonce`);
 
-
-      const messageToSign = JSON.stringify(seedResponse.data.seed)
+      const messageToSign = JSON.stringify(seedResponse.data.seed);
 
       const signed = await (window as any).ethereum.request({
-        method: 'personal_sign',
-        params: [messageToSign, account],
+        method: "personal_sign",
+        params: [messageToSign, account]
       });
 
       const res = await axios.post(`${API_URL}/login`, {
         message: messageToSign,
         signedMessage: signed,
-        address: account,
+        address: account
       });
 
-
-      const res2 = await axios.get(`${API_URL}/${"0x0e394F52d5D8bb6a30A14E65d6E8D2314198fBb9"}/download`, {
-        headers: {
-          "x-access-token": res.data
+      const res2 = await axios.get(
+        `${API_URL}/${"0x6f07465bD94A4e12ee77905EB6477497f0AaBcA7"}/download`,
+        {
+          headers: {
+            "x-access-token": res.data
+          }
         }
-      })
+      );
 
-      console.log(res2)
+      console.log(res2);
 
       setJWT(res.data);
-      dispatchUser({ type: 'LOGIN' });
-
+      dispatchUser({ type: "LOGIN" });
     } catch (error) {
       console.log(error);
     }
@@ -71,10 +83,14 @@ export const UserContextProvider = ({ children }: { children: React.ReactNode })
   const logout = () => {
     setUserAddress(null);
     setJWT(null);
-    dispatchUser({ type: 'LOGOUT' });
+    dispatchUser({ type: "LOGOUT" });
   };
 
-  return <UserContext.Provider value={{ isLoggedIn, address, login, logout, jwt }}>{children}</UserContext.Provider>;
+  return (
+    <UserContext.Provider value={{ isLoggedIn, address, login, logout, jwt }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export const useUserContext = () => {
