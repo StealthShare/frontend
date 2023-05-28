9ccc657335a29107d9142549bccb39e9e566f342
Patryk Rossa <patryk.rossaa@gmail.com>
2023-05-28 06:05:32 +0200

    inventory, marketplace search

--- src/components/pages/marketplace/leftSection/LeftSection.tsx
+++ src/components/pages/marketplace/leftSection/LeftSection.tsx
@@ -1,11 +1,17 @@
-import { Button, Flex, Grid, InputGroup, InputRightElement } from '@chakra-ui/react';
-import React, { FC, useEffect, useState } from 'react';
-import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
-import { SearchIcon } from '../../../../icons/SearchIcon';
-import { useListingContext } from '../../../../provider/listings/ListingsContext';
-import { CustomInput } from '../../../shared/CustomInput';
-import { FileItem } from '../../../shared/FileItem';
-import { SmallGridFile } from '../rightSection/SmallGridFile';
+import {
+  Button,
+  Flex,
+  Grid,
+  InputGroup,
+  InputRightElement
+} from "@chakra-ui/react";
+import React, { FC, useEffect, useState } from "react";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { SearchIcon } from "../../../../icons/SearchIcon";
+import { useListingContext } from "../../../../provider/listings/ListingsContext";
+import { CustomInput } from "../../../shared/CustomInput";
+import { FileItem } from "../../../shared/FileItem";
+import { SmallGridFile } from "../rightSection/SmallGridFile";
 
 interface ILeftSectionProps {
   activeGrid: string;
@@ -30,74 +36,91 @@ export const LeftSection: FC<ILeftSectionProps> = ({
   const navigate = useNavigate();
 
   const handleInputChange = (e: any) => {
-    if(listings) {
-
-    
-    setInputValue(e.target.value);
-    console.log(
-      listings
-        .map((listing: any) => listing.name)
-        .filter((name: string) => {
-          return name.includes(e.target.value);
-        })
-    );
-    const matchingNames = listings
-      .map((listing: any) => listing.name)
-      .filter((name: string) => {
-        return name.includes(e.target.value);
-      });
-    setFilteredListings(listings.filter((listing: any) => matchingNames.includes(listing.name)));
-  }
-  };
-
-  useEffect(() => {
-    if (search.get('search') !== null) {
-      setInputValue(search.get('search')!);
+    if (listings) {
+      setInputValue(e.target.value);
+      console.log(
+        listings
+          .map((listing: any) => listing.name)
+          .filter((name: string) => {
+            return name.includes(e.target.value);
+          })
+      );
       const matchingNames = listings
         .map((listing: any) => listing.name)
         .filter((name: string) => {
-          return name.includes(search.get('search') as string);
+          return name.includes(e.target.value);
         });
-      setFilteredListings(listings.filter((listing: any) => matchingNames.includes(listing.name)));
+      setFilteredListings(
+        listings.filter((listing: any) => matchingNames.includes(listing.name))
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (search.get("search") !== null) {
+      setInputValue(search.get("search")!);
+      const matchingNames = listings
+        .map((listing: any) => listing.name)
+        .filter((name: string) => {
+          return name.includes(search.get("search") as string);
+        });
+      setFilteredListings(
+        listings.filter((listing: any) => matchingNames.includes(listing.name))
+      );
     } else {
-      console.log('No search');
+      console.log("No search");
       setFilteredListings(listings);
     }
   }, [search]);
 
   return (
     <Flex flexDir="column" gap="30px">
-      <InputGroup>
+      {/* <InputGroup> */}
+      <Flex>
         <CustomInput
+          borderRightRadius="0"
           placeholder={placeholder}
           value={inputValue}
-          defaultValue={search.get('search')}
+          defaultValue={search.get("search")}
           onChange={(e: any) => handleInputChange(e)}
           onKeyDown={(e: any) => {
-            console.log(e.key === 'Enter');
-            if (e.key === 'Enter' && inputValue.length > 0) navigate(`/marketplace?search=${inputValue}`);
+            console.log(e.key === "Enter");
+            if (e.key === "Enter" && inputValue.length > 0)
+              navigate(
+                `/${
+                  download ? "inventory" : "marketplace"
+                }?search=${inputValue}`
+              );
           }}
         />
-        <InputRightElement h="100%">
-          <Button
-            mr="16px"
-            borderRadius="50%"
-            bg="rgba(186, 116, 248, 0.5)"
-            onClick={() => {
-              if (inputValue.length > 0) navigate(`/marketplace?search=${inputValue}`);
-            }}
-          >
-            <SearchIcon />
-          </Button>
-        </InputRightElement>
-      </InputGroup>
-
-      <Grid gap="20px" flexWrap="wrap" templateColumns={(activeGrid as any) == 'small' ? '1fr' : 'repeat(4, 1fr)'}>
-        {filteredListings.map((item :any, index : any) => {
+        <Button
+          borderRightRadius="8px"
+          borderLeftRadius="0"
+          boxSize="69px"
+          onClick={() => {
+            if (inputValue.length > 0)
+              navigate(
+                `/${
+                  download ? "inventory" : "marketplace"
+                }?search=${inputValue}`
+              );
+          }}
+        >
+          <SearchIcon />
+        </Button>
+      </Flex>
+      <Grid
+        gap="20px"
+        flexWrap="wrap"
+        templateColumns={
+          (activeGrid as any) == "small" ? "1fr" : "repeat(4, 1fr)"
+        }
+      >
+        {filteredListings.map((item: any, index: any) => {
           return (
             <FileItem
               key={item._id}
-              isSmall={(activeGrid as any) == 'big'}
+              isSmall={(activeGrid as any) == "big"}
               type={item.type}
               category="Games"
               imageUrl={item.image}
