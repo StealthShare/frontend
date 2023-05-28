06bcd5e3415e79b5f11e06d8f621ec4897829997
damiansarnecki <xdamiansarnecki@gmail.com>
2023-05-28 13:58:32 +0200

    save

--- src/pages/Inventory.tsx
+++ src/pages/Inventory.tsx
@@ -1,6 +1,7 @@
 import { Flex, Grid } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import useLocalStorage from "use-local-storage";
 import { LeftSection } from "../components/pages/marketplace/leftSection/LeftSection";
 import { RightSection } from "../components/pages/marketplace/rightSection/RightSection";
 import { PageContainer } from "../components/shared/containers/PageContainer";
@@ -12,10 +13,7 @@ export const Inventory = () => {
   const [activeGrid, setActiveGrid] = useState<string>("big");
   const { jwt } = useUserContext();
   const navigate = useNavigate();
-  const inventory =
-    localStorage.getItem("inventory") !== null
-      ? JSON.parse(localStorage.getItem("inventory")!.toString())
-      : [];
+  const [inventory, setInventory] = useLocalStorage("inventory", [])
   const [listings, setListings] = useState<any>();
   const [filteredListings, setFilteredListings] = useState<any[]>(listings);
   const { listings: l } = useListingContext();
@@ -24,20 +22,7 @@ export const Inventory = () => {
     if (jwt === null) navigate("/");
   }, [jwt]);
 
-  useEffect(() => {
-    var pom: any[] = [];
-
-    const addresses = inventory.map((item: any) => item.token);
-
-    const x = l.filter((l) => addresses.includes(l.token));
-    console.log(x);
-    inventory?.forEach((i: any) => {
-      l.forEach((listing: any) => {
-        if (addresses.includes(i.token)) pom.push(listing);
-      });
-    });
-    setListings(x);
-  }, [l]);
+  
 
   return (
     <PageContainer>
@@ -54,7 +39,7 @@ export const Inventory = () => {
             placeholder={"Search in your inventory"}
             activeGrid={activeGrid}
             download
-            filteredListings={listings}
+            filteredListings={l}
             setFilteredListings={setFilteredListings}
           />
         </Grid>
